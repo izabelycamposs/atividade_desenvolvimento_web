6c83ff4412058302f1028be85fa68f53f333de37
izabelycamposs <162514098+izabelycamposs@users.noreply.github.com>
2024-05-11 12:15:31 -0300

    calcular media

---
+++ media/js/media.js
@@ -0,0 +1,26 @@
+function calcularMedia(){
+    var inNome = document.getElementById("inNome");
+    var inNota1 = document.getElementById("inNota1");
+    var inNota2 = document.getElementById("inNota2");
+    var outSituacao = document.getElementById("outSituacao");
+    var outMedia = document.getElementById("outMedia");
+
+    var nome = inNome.value;
+    var nota1 = Number(inNota1.value);
+    var nota2 = Number(inNota2.value);
+
+    var media = (nota1 + nota2)/2;
+
+    outMedia.textContent = "Media das Notas:" + media.toFixed(1);
+    if(media>=7){
+        outSituacao.textContent="Parabens " + nome + "! Você foi aprovado";
+        outSituacao.style.color="blue";
+    }
+    else{
+        outSituacao.textContent = "Ops " + nome + "...Você foi reprovado(a)";
+        outSituacao.style.color="red";
+    }
+}
+    var btResultado = document.getElementById("btResultado");
+
+btResultado.addEventListener("click", calcularMedia);
